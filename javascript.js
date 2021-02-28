fde5931fb2d8d43be3caa0d2bce51e0d6a4cde27
Yasminjama <77639257+Yasminjama@users.noreply.github.com>
2021-02-28 13:33:32 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function h1(){
+document.getElementById("h1").style.color="chocolate";
+document.getElementById("h1").style.fontFamily="brush script m7", Brush, cursive;
+document.getElementById("h1").style.textAlign="center";
+}
+function myFirstFunction() {
+  console.log("Välkommen!");
+}
